b32426a2b3bd1567af53e03ae6b9fede293b1181
devRena <renatofelipeherrera.c@gmail.com>
2026-06-28 22:23:06 -0400

    Setup MongoDB connection, Task model, and GET endpoint to retrieve all tasks

---
+++ db/tasks.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const taskSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true },
+);
+
+export default mongoose.model("Tasks", taskSchema);
--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 import express from "express";
 import { PORT } from "./config.js";
 import morgan from "morgan";
+import tasks from "./db/tasks.js";
+import connectDB from "./db/db.js";
+
+connectDB();
 
 const app = express();
 app.use(express.json());
@@ -8,6 +12,12 @@ app.use(morgan("dev"));
 
 app.get("/", (req, res) => {
   res.json({ message: "Hello world" });
+});
+
+// GET ALL TASKS
+app.get("/api/tasks", async (req, res) => {
+  const task = await tasks.find();
+  return res.status(200).json(task);
 });
 
 app.listen(PORT, () => {
